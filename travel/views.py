b436ea6d4f4478a2230e8601a2a275d6deba9bc7
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-06-14 19:51:40 +0000

    Wed, Jun 14, 2023, 3:51 PM -04:00

--- travel/views.py
+++ travel/views.py
@@ -32,7 +32,7 @@ def login_view(request):
                 "message": "Invalid username and/or password."
             })
     else:
-        return render(request, "network/login.html")
+        return render(request, "capstone/login.html")
 
 
 def logout_view(request):
@@ -49,7 +49,7 @@ def register(request):
         password = request.POST["password"]
         confirmation = request.POST["confirmation"]
         if password != confirmation:
-            return render(request, "network/register.html", {
+            return render(request, "capstone/register.html", {
                 "message": "Passwords must match."
             })
 
@@ -58,10 +58,10 @@ def register(request):
             user = User.objects.create_user(username, email, password)
             user.save()
         except IntegrityError:
-            return render(request, "network/register.html", {
+            return render(request, "capstone/register.html", {
                 "message": "Username already taken."
             })
         login(request, user)
         return HttpResponseRedirect(reverse("index"))
     else:
-        return render(request, "network/register.html")
+        return render(request, "capstone/register.html")
