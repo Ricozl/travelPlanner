b6ece4bb5abbeea8203b39a3125c7cb02dd62478
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-06-17 20:01:09 +0000

    Sat, Jun 17, 2023, 4:01 PM -04:00

--- capstone/static/capstone/index.js
+++ capstone/static/capstone/index.js
@@ -4,4 +4,4 @@ document.addEventListener('DOMContentLoaded', function (event) {
     //load_page('all_posts');
     console.log("got to js page")
     document.querySelector("#message").innerHTML = "Got to index.js"
-};
+});
