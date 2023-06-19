638bb0a8b9cd8ca27297fa228da3225af72e8e7c
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-06-19 15:52:20 +0000

    Mon, Jun 19, 2023, 11:52 AM -04:00

--- capstone/static/capstone/index.js
+++ capstone/static/capstone/index.js
@@ -4,11 +4,14 @@ document.addEventListener('DOMContentLoaded', function (event) {
     //load_page('all_posts');
     console.log("got to js page")
     //document.querySelector("#message").innerHTML = "Got to index.js"
+
+    // toggle between buttons
+    document.querySelector('#food').addEventListener('click', compose_email);
 });
 
 function compose_email() {
     // Show compose view and hide other views
-    document.querySelector('#emails-view').style.display = 'none';
+    document.querySelector('#img1').style.display = 'none';
     document.querySelector('#emails-list').style.display = 'none';
     document.querySelector('#oneEmail').style.display = 'none';
     document.querySelector('#compose-view').style.display = 'block';
