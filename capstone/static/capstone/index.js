c4402979a6d35eaec562c7ac73b8d7d969615f91
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-06-23 23:15:09 +0000

    Fri, Jun 23, 2023, 7:15 PM -04:00

--- capstone/static/capstone/index.js
+++ capstone/static/capstone/index.js
@@ -1,6 +1,11 @@
 const apikey = 'pk.eyJ1IjoibWFwLXVuNjIzIiwiYSI6ImNsajk1bmtuajB6bWQzbW81dXQxc2V3ZmsifQ.pHR1ndWInokMfvttpqGnag';
 var map = L.map('map').setView([51.505, -0.09], 13);
 
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
 document.addEventListener('DOMContentLoaded', function (event) {
     // by default, load all posts
     event.preventDefault();
