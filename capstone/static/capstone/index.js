a888a0a8847b91fef76e6cfc22b210178d35cf86
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-06-24 16:46:05 +0000

    Sat, Jun 24, 2023, 12:46 PM -04:00

--- capstone/static/capstone/index.js
+++ capstone/static/capstone/index.js
@@ -31,12 +31,12 @@ function pick_place() {
     const mymap = L.map('map').setView([51.505, -0.09], 13);
 
     //L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}, {
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         maxZoom: 19,
         id: 'mapbox/streets=v11',
         tileSize: 512,
         zoomOffset: -1,
         accessToken: apikey,
-    }).addTo(map);
+    }).addTo(mymap);
 
 };
