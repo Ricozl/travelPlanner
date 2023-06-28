4c9534895ace6aeeec1e712bad299510afc5a3e2
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-06-28 19:29:22 +0000

    Wed, Jun 28, 2023, 3:29 PM -04:00

--- capstone/models.py
+++ capstone/models.py
@@ -4,3 +4,16 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Sites(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=512)
+    image_url = models.URLField(max_length=1024, blank=True)
+    sites_neighborhood = models.ForeignKey(Neighborhoods, on_delete=models.CASCADE, related_name="sites_hood")
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return "Sites title: {}, Sites Category: {}, Description: {}, Image URL: {}".format(self.title, self.sites_category.cat_name, self.description, self.image_url)
