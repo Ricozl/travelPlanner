abb6bd0b9a6077722e40e91fcd2a17da28c0ecfe
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-07-01 15:13:56 +0000

    Sat, Jul 1, 2023, 11:13 AM -04:00

--- capstone/models.py
+++ capstone/models.py
@@ -5,7 +5,15 @@ from django.db import models
 class User(AbstractUser):
     pass
 
+class Categories(models.Model):
+    cat_name = models.CharField(max_length=64, blank=True, null=True)
 
+    class Meta:
+        ordering = ['cat_name']
+
+    def __str__(self):
+        return self.cat_name
+    
 class Sites(models.Model):
     title = models.CharField(max_length=64)
     description = models.CharField(max_length=512)
