a847b7394302a4a0e9c75226bf6e2ef3d98525c9
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-07-02 21:59:13 +0000

    Sun, Jul 2, 2023, 5:59 PM -04:00

--- capstone/static/capstone/index.js
+++ capstone/static/capstone/index.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function (event) {
     //document.querySelector('#jumpto').addEventListener('click', choose_site);
     //document.querySelector('#destinations').addEventListener('click', pick_place);
     //document.querySelector('#food').addEventListener('click', compose_email);
-    document.querySelector('#ancient-rome').addEventListener('click', )
+    document.querySelector('#ancient-rome').addEventListener('click', old_rome);
 });
 
 function compose_email() {
