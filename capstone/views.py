60c1b9bcdb9dd9c8880ecb941c3b841a16775668
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-07-03 23:23:13 +0000

    Mon, Jul 3, 2023, 7:23 PM -04:00

--- capstone/views.py
+++ capstone/views.py
@@ -32,7 +32,7 @@ def sites(request):
    #need to get int for site from categories first, then use it to look up in Sites
 
     # this gives good data but goes to blank page
-    data = list(Sites.objects.filter(
+    sites = list(Sites.objects.filter(
         sites_category__cat_name=site).values())
         #sites_category__cat_name=title, is_active="True").values()
     #data_json = json.dumps(list(sites))
@@ -41,7 +41,7 @@ def sites(request):
 
     #data = serializers.serialize("json", SomeModel.objects.all())
 
-    return JsonResponse(data, safe=False)
+    return JsonResponse(sites, safe=False)
     #return JsonResponse([site.serialize() for site in sites], safe=False)
     catno = Categories.objects.get(cat_name = sitenm)
     print(catno)
