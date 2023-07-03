1d70e06cdf5fbb62ff99f7168fe42e2124be25aa
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-07-03 20:09:19 +0000

    Mon, Jul 3, 2023, 4:09 PM -04:00

--- capstone/static/capstone/index.js
+++ capstone/static/capstone/index.js
@@ -51,7 +51,6 @@ function load_site(site) {
     fetch(`/sites/${site}`)
         .then(response => response.json())
         .then(sites => {
-            console.log(sites)
             if (sites.error) {
                 console.log(sites.error)
                 document.querySelector('#siteName').innerHTML = "Error: " + sites.error;
