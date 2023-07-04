5d4d790ae069b02a39e2e5e19425531dd2c87457
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-07-04 18:31:29 +0000

    Tue, Jul 4, 2023, 2:31 PM -04:00

--- capstone/static/capstone/index.js
+++ capstone/static/capstone/index.js
@@ -65,18 +65,12 @@ function load_site(site) {
         .then(data => {
             console.log('got to fetch sites')
             console.log(data)
-            console.log(data.title)
-            if (data.error) {
-                console.log(data.error)
-                document.querySelector('#siteName').innerHTML = "Error: " + data.error;
-            }
-            else {
 
             // create separate div for each email
-                const element = document.createElement('div');
+            const element = document.createElement('div');
 
-                // build each email
-                element.innerHTML = `<p style="display:inline-block; width:20%;">${data.title}</p><p style="width:50%;">${data.description}</p><p>${data.category}`;
+            // build each email
+            element.innerHTML = `<p style="display:inline-block; width:20%;">${data.title}</p><p style="width:50%;">${data.description}</p><p>${data.category}`;
 
             // add event listener for clicking on a site
             //element.addEventListener('click', function(e) {
