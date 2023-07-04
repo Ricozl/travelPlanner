2193d0f27fcf9944cbe6836705f0c1d5e1fb6e49
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-07-04 20:21:05 +0000

    Tue, Jul 4, 2023, 4:21 PM -04:00

--- capstone/static/capstone/index.js
+++ capstone/static/capstone/index.js
@@ -80,7 +80,7 @@ function load_site(site) {
             element.appendChild(img);
 
             // build each email
-            element.innerHTML = `<h5 style="width:20%;">${title}</h5><p style="display:inline-block;"><img src="image"></p><p style="display:inline-block; width:50%;">${description}</p><span><img src="https://www.publicdomainpictures.net/pictures/40000/velka/red-heart-1362916005N5Z.jpg" style="width:20px; height:20px;"></span>`;
+            element.innerHTML = `<h5 style="width:20%;">${title}</h5><p style="display:inline-block;"><p style="display:inline-block; width:50%;">${description}</p><span><img src="https://www.publicdomainpictures.net/pictures/40000/velka/red-heart-1362916005N5Z.jpg" style="width:20px; height:20px;"></span>`;
 
             // add event listener for clicking on a site
             //element.addEventListener('click', function(e) {
