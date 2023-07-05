d8e5b4ea6dc1bdadb60a2a650ce5eceece90295a
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-07-05 17:29:29 +0000

    Wed, Jul 5, 2023, 1:29 PM -04:00

--- capstone/static/capstone/index.js
+++ capstone/static/capstone/index.js
@@ -69,7 +69,6 @@ function load_site(site) {
                 title = el.title;
                 console.log(el.title);
                 console.log(el.image_url)
-                console.log(image)
                 description = el.description;
                 category = el.sites_category;
             })
