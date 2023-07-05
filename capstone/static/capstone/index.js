25319e6fb1b7c9e6f67ea2628dd0e7f086d41133
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-07-05 22:58:04 +0000

    Wed, Jul 5, 2023, 6:58 PM -04:00

--- capstone/static/capstone/index.js
+++ capstone/static/capstone/index.js
@@ -68,8 +68,8 @@ function load_site(site) {
             data.forEach((el) => {
                 title = el.title;
                 console.log(el.title);
-                image = el.image;
-                console.log(image);
+                image = el.image_url;
+                console.log(image_url);
                 description = el.description;
                 category = el.sites_category;
             })
@@ -86,7 +86,7 @@ function load_site(site) {
                 //img.src = "https://static/capstone/campo-dei-fiori2.jpg"
             //}
             // build each email
-            element.innerHTML = `<h5 style="width:20%;">${title}</h5><p style="display:inline-block;"><img src="{ image }" style="width:400px; height:400px;">${el.image}<p style="display:inline-block; width:50%;">${description}</p><span><img src="https://www.publicdomainpictures.net/pictures/40000/velka/red-heart-1362916005N5Z.jpg" style="width:20px; height:20px;"></span>`;
+            element.innerHTML = `<h5 style="width:20%;">${title}</h5><p style="display:inline-block;"><img src="{ image }" style="width:400px; height:400px;">${image}<p style="display:inline-block; width:50%;">${description}</p><span><img src="https://www.publicdomainpictures.net/pictures/40000/velka/red-heart-1362916005N5Z.jpg" style="width:20px; height:20px;"></span>`;
 
             // add event listener for clicking on a site
             //element.addEventListener('click', function(e) {
