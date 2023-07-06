8da4b58aab9c0b3520564131da009a6766393258
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-07-06 16:30:12 +0000

    Thu, Jul 6, 2023, 12:30 PM -04:00

--- capstone/admin.py
+++ capstone/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import Categories, Sites, Favorites
 
 # Register your models here.
+class SitesAdmin(admin.ModelAdmin):
+    list_display = ("title")
+
 admin.site.register(Categories)
-admin.site.register(Sites)
+admin.site.register(Sites, SitesAdmin)
 admin.site.register(Favorites)
