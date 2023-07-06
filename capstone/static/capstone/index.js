a485fab884b5a27d35c7cf90eff0013a4d15ad71
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-07-06 21:33:14 +0000

    Thu, Jul 6, 2023, 5:33 PM -04:00

--- capstone/static/capstone/index.js
+++ capstone/static/capstone/index.js
@@ -85,7 +85,7 @@ function load_site(site) {
 
             //document.querySelector('#sites').append(element);
             var opt = document.createElement("img");
-            opt.setAttribute('src', 'el.image_url');
+            opt.setAttribute('src', 'static/capstone/campo-dei-fiori2.jpg');
             //opt.setAttribute('Pick a date', 'alternate text');
             opt.setAttribute('height', '300px');
             opt.setAttribute('width', '300px');
@@ -99,7 +99,7 @@ function load_site(site) {
             //}
             // build each email
             const ele = document.createElement('p');
-          
+
             //
             ele.innerHTML = `<p style="display:inline-block; width:50%;">${description}</p><span><img src="https://www.publicdomainpictures.net/pictures/40000/velka/red-heart-1362916005N5Z.jpg" style="width:20px; height:20px;"></span>`;
             //ele.innerHTML = `<h5 style="width:20%;">${title}</h5><p style="display:inline-block;"><img src="{{img.src}}" style="width:400px; height:400px;"><p style="display:inline-block; width:50%;">${description}</p><span><img src="https://www.publicdomainpictures.net/pictures/40000/velka/red-heart-1362916005N5Z.jpg" style="width:20px; height:20px;"></span>`;
