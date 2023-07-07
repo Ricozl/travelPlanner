89c20e022f8f6649a5920b894e67ad79a53cf352
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-07-07 19:23:15 +0000

    Fri, Jul 7, 2023, 3:23 PM -04:00

--- capstone/static/capstone/index.js
+++ capstone/static/capstone/index.js
@@ -43,6 +43,7 @@ function choose_site() {
 function load_site(site) {
     console.log('got to load_site')
     // Show the site and hide other views
+    document.querySelector('#top-site').innerHTML = "";
     document.querySelector('#showImgs').style.display = 'none';
     document.querySelector('#top-site').style.display = 'block';
     //document.querySelector('#emails-list').style.display = 'block';
