0b99dd705200166ac2f9b0d8244216552cc3e63a
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-07-07 20:13:22 +0000

    Fri, Jul 7, 2023, 4:13 PM -04:00

--- capstone/static/capstone/index.js
+++ capstone/static/capstone/index.js
@@ -41,10 +41,14 @@ function choose_site() {
 };
 
 function load_site(site) {
+    window.addEventListener('load',
+  function() {
+    alert('hello!');
+  }, false);
     console.log('got to load_site')
     // Show the site and hide other views
     document.querySelector('#sites').innerHTML = "";
-    //document.querySelector('#messages').innerHTML = " ";
+    document.querySelector('#messages').innerHTML = "";
     document.querySelector('#showImgs').style.display = 'none';
     document.querySelector('#top-site').style.display = 'block';
     //document.querySelector('#emails-list').style.display = 'block';
