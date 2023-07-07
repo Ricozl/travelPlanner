4e0c463ca56bb282e8f5b9aa3ee1a1077e420500
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-07-07 15:07:32 +0000

    Fri, Jul 7, 2023, 11:07 AM -04:00

--- capstone/static/capstone/index.js
+++ capstone/static/capstone/index.js
@@ -91,11 +91,11 @@ function load_site(site) {
             //ele.innerHTML = `<h5 style="width:20%;">${title}</h5><p style="display:inline-block;"><img src="{{img.src}}" style="width:400px; height:400px;"><p style="display:inline-block; width:50%;">${description}</p><span><img src="https://www.publicdomainpictures.net/pictures/40000/velka/red-heart-1362916005N5Z.jpg" style="width:20px; height:20px;"></span>`;
             element.appendChild(ele);
             // add event listener for clicking on a site
-            //element.addEventListener('click', function(e) {
-                //const element = e.target;
-                //console.log(element)
+            element.addEventListener('click', function(e) {
+                const etarget = e.target;
+                console.log(etarget)
 
-        //});
+            });
             document.querySelector('#sites').append(element);
         })
         .catch(error => {
