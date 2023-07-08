b4b3f28c332c309b551c0e84a3ce7d4776e14f56
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-07-08 18:50:37 +0000

    Sat, Jul 8, 2023, 2:50 PM -04:00

--- capstone/static/capstone/index.js
+++ capstone/static/capstone/index.js
@@ -77,7 +77,6 @@ function load_site(site) {
 
                     var opt = document.createElement("img");
                     opt.setAttribute('src', data[i].image_url);
-                    opt.setAttribute('id', 'site-img');
                     //opt.setAttribute('Pick a date', 'alternate text');
                     opt.setAttribute('height', '80%');
                     opt.setAttribute('width', '80%');
