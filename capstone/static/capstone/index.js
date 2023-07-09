0904c2da6f81d2b1c8ee214a11f8360ada126a1d
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-07-09 19:49:43 +0000

    Sun, Jul 9, 2023, 3:49 PM -04:00

--- capstone/static/capstone/index.js
+++ capstone/static/capstone/index.js
@@ -49,7 +49,7 @@ function load_site(site) {
 
     //document.querySelector('#messages').innerHTML = "";
     document.querySelector('#showImgs').style.display = 'none';
-    document.querySelector('#nav-pics').style.display = 'none';
+    //document.querySelector('#nav-pics').style.display = 'none';
     document.querySelector('#top-site').style.display = 'block';
     //document.querySelector('#emails-list').style.display = 'block';
     //document.querySelector('#oneEmail').style.display = 'none';
