6098b42caf996e4a41331dcd85331dd3385cbb5f
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-07-16 22:18:59 +0000

    Sun, Jul 16, 2023, 6:18 PM -04:00

--- capstone/static/capstone/index.js
+++ capstone/static/capstone/index.js
@@ -148,6 +148,23 @@ function favorites() {
     console.log('got to favorites');
 }
 
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+const csrftoken = getCookie('csrftoken');
+
 function pick_place() {
     console.log('Got to pick_place')
     // show newPage and hide main page
