9201f1943658bb4e3d5e5b9706dec4dafc15e5ab
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-07-16 20:05:08 +0000

    Sun, Jul 16, 2023, 4:05 PM -04:00

--- capstone/static/capstone/index.js
+++ capstone/static/capstone/index.js
@@ -120,7 +120,7 @@ function load_site(site) {
 function updateRecord(title) {
     // update 'content' in database
     //post_id = parseInt(post_id)
-    fetch(`/favorites/${title}`, {
+    fetch(`/updateRecord/${title}`, {
         method: 'PUT',
         //headers: {'X-CSRFToken': csrftoken},
         mode: 'same-origin',
