e2bb020f20a4bba4983b18654569b26f786da590
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-07-16 18:52:40 +0000

    Sun, Jul 16, 2023, 2:52 PM -04:00

--- capstone/static/capstone/index.js
+++ capstone/static/capstone/index.js
@@ -99,7 +99,7 @@ function load_site(site) {
                         const element = e.target;
                         console.log(element)
                         element.previousSibling.innerText = "Added Successfully!"
-                        console.log({data[i].title})
+                        console.log(${data[i].title})
                         //console.log({data[i].id})
                         // update favorites table in database, set to active
                     });
