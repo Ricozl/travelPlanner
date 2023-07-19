5f753d783b6d2391835acf6feaa9ed76d69de350
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-07-19 20:31:34 +0000

    Wed, Jul 19, 2023, 4:31 PM -04:00

--- capstone/views.py
+++ capstone/views.py
@@ -45,7 +45,7 @@ def sites(request, site):
         #sites = Sites.objects.filter(pk__exact=item_id=favSites)
         sites = Favorites.objects.filter(
             #watcher=wat_user, is_active=True).select_related('item').order_by('item')
-            watcher=wat_user, is_active=True).values('item')
+            watcher=wat_user, is_active=True).select_related('item')
         # display user's favorites list
         print(sites)
 
