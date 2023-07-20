db36e388730f81921e9ed38457f2af41dea345f2
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-07-20 21:02:26 +0000

    Thu, Jul 20, 2023, 5:02 PM -04:00

--- capstone/views.py
+++ capstone/views.py
@@ -40,6 +40,8 @@ def sites(request, site):
             watcher=wat_user, is_active=True).values_list('item')
             #watcher=wat_user, is_active=True)
         print(favSites)
+
+        for i < len(favSites):
         sites = Sites.objects.filter(title__fav_list__in=favSites)
         print(sites)
 
