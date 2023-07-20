631d9e25ae5b24fc13c471047aa650f406ee8f5a
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-07-20 21:57:46 +0000

    Thu, Jul 20, 2023, 5:57 PM -04:00

--- capstone/views.py
+++ capstone/views.py
@@ -36,11 +36,11 @@ def sites(request, site):
 
         #favors = fav_list.sites.all()
         #print(favors)
-        sitelist = Favorites.objects.filter(
+        sites = Favorites.objects.filter(
             #watcher=wat_user, is_active=True).values_list('item')
             watcher=wat_user, is_active=True)
         #print(favSites)
-        print(sitelist)
+        print(sites)
         ##print(favSites[1].watcher)
 
         #followedNms = Follow.objects.filter(
@@ -67,8 +67,11 @@ def sites(request, site):
         # display user's favorites list
         #print(sites)
 
-        sites = json.dumps(list(sitelist))
-        return HttpResponse({"sites": sites}, safe=False)
+        return render(request, "capstone/favorites.html", {
+            "sites": sites
+        })
+        #sites = json.dumps(list(sitelist))
+        #return HttpResponse({"sites": sites}, safe=False)
         #return JsonResponse([site.serialize() for site in sites], safe=False)
     else:
         print(site)
