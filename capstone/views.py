6cfa58f2fb530fa12b128b22c6d393a8d8009300
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-07-20 21:06:31 +0000

    Thu, Jul 20, 2023, 5:06 PM -04:00

--- capstone/views.py
+++ capstone/views.py
@@ -40,9 +40,12 @@ def sites(request, site):
             watcher=wat_user, is_active=True).values_list('item')
             #watcher=wat_user, is_active=True)
         print(favSites)
-
+        sites=[]
+        var i = 0;
         for i < len(favSites):
-        sites = Sites.objects.filter(title__fav_list__in=favSites)
+            e = Sites.objects.filter(title__fav_list[i]__in=favSites)
+            sites.append(e)
+            i = i + 1
         print(sites)
 
         #sites = Sites.objects.filter(pk__exact=item_id=favSites)
@@ -52,7 +55,7 @@ def sites(request, site):
             #item=favSites.title, is_active="True").values()
         #print(active_list)
         #pubs = publication.objects.select_related('country', 'country_state', 'city')
-        favs = set()
+        #favs = set()
 
         #sites = Favorites.objects.filter(
             #watcher=wat_user, is_active="True").select_related('item')
