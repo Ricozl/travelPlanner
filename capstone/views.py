086556b458d00a19eebc892c870a6a3b4fcf66b4
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-07-20 21:28:49 +0000

    Thu, Jul 20, 2023, 5:28 PM -04:00

--- capstone/views.py
+++ capstone/views.py
@@ -62,7 +62,7 @@ def sites(request, site):
         #print(sites)
 
         #sites = json.dumps(list(siteList))
-        return HttpResponse({"sites": "favSites"})
+        return HttpResponse({"sites": favSites})
         #return JsonResponse([site.serialize() for site in sites], safe=False)
     else:
         print(site)
