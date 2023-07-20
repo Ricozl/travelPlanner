6528152c91a474831087b05c18b59ed9ae56034c
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-07-20 21:42:26 +0000

    Thu, Jul 20, 2023, 5:42 PM -04:00

--- capstone/views.py
+++ capstone/views.py
@@ -40,6 +40,7 @@ def sites(request, site):
             #watcher=wat_user, is_active=True).values_list('item')
             watcher=wat_user, is_active=True)
         print(favSites)
+        print(favSites[0])
 
         #followedNms = Follow.objects.filter(
             #follower=usernm, is_active=True).values_list('followed')
