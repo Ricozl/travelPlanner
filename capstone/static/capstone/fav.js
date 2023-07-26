48d8eb5a2381b1336381af410cdfdb7d48994bef
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-07-26 20:42:54 +0000

    Wed, Jul 26, 2023, 4:42 PM -04:00

---
+++ capstone/static/capstone/fav.js
@@ -0,0 +1,4 @@
+<script src="https://unpkg.com/react@17/umd/react.production.min.js" crossorigin></script>
+<script src="https://unpkg.com/react-dom@17/umd/react-dom.production.min.js" crossorigin></script>
+<script src="https://unpkg.com/babel-standalone@6/babel.min.js"></script>
+
