53fd015ea8be07ca013ec14e71e855ba92ee9230
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-07-29 20:45:29 +0000

    Sat, Jul 29, 2023, 4:45 PM -04:00

--- capstone/static/capstone/form.js
+++ capstone/static/capstone/form.js
@@ -3,8 +3,24 @@ import React, {useState} from 'react';
 export default function App() {
     const questions = [
         {
-            questionText: "What is the capital of France?"
-        }
+            questionText: "What is the capital of France?",
+            answerOptions: [
+                { answerText: 'New York', isCorrect: false},
+                { answerText: 'London', isCorrect: false },
+                { answerText: 'Paris', isCorrect: true },
+                { answerText: 'Dublin', isCorrect: false }
+            ],
+
+        },
+        {
+            questionText: 'Who is CEO of Tesla?',
+            answerOptions: [
+                { answerText: 'Jeff Bezos', isCorrect: false },
+                { answerText: 'Elon Musk', isCorrect: true },
+                { answerText: 'Bill Gates', isCorrect: false },
+                { answerText: 'Tony Stark', isCorrect: false }
+            ],
+        },
     ]
 }
 
