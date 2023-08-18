6348e13f58b07dd49d02269ede1846dc8f88324d
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-08-18 17:18:06 +0000

    Fri, Aug 18, 2023, 1:18 PM -04:00

--- project5/settings.py
+++ project5/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = '/capstone/capstone/static/'
 
-#STATICFILES_DIRS = [os.path.join(BASE_DIR,'/static/'),]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static'),]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
