efcd9e25b2230c1522ac3c524e28e66187a9cd76
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-09-15 23:48:41 +0000

    Fri, Sep 15, 2023, 7:48 PM -04:00

--- capstone/static/capstone/index.js
+++ capstone/static/capstone/index.js
@@ -25,7 +25,7 @@ function load_site(site) {
     fetch(`/sites/${site}`, {
         method: 'GET',
         headers: {
-            'Content-Type': 'application/json',    
+            'Content-Type': 'application/json',
         }
     })
         .then(response => response.json())
@@ -135,49 +135,5 @@ function getCookie(name) {
     }
     return cookieValue;
 };
-
-
-//function form() {
-    //const [useState] = React
-    //const [email, setEmail] = useState("")
-    //const [password, setPassword] = useState("")
-
-    //function handleSubmit(e) {
-        //e.preventDefault()
-        //console.log("email", email)
-        //console.log("password", password)
-    //}
-    //return (
-        //<form className="at-8 space-y-6" onSubmit=[handleSubmit]>
-            //<div>
-                //<label for="email-address"
-                    //className="sr-only">Email Address</label>
-                //<input id="email-address"
-                    //name="email"
-                    //type="email"
-                    //autocomplete="off"
-                    //required
-                    //placeholder="Email address"
-                    //value=[email]
-                    //onInput=[(e) => setEmail(e.target.value)]
-                    ///> is this one wrong?
-            //</div>
-            //<div>
-                //<label for="password"
-                    //className="sr-only">Password</label>
-                //<input id="password"
-                    //name="password"
-                    //type="password"
-                    //autocomplete="off"
-                    //required
-                    //placeholder="Password"
-                    //value=[password]
-                    //onInput=[(e) => setPassword(e.target.value)]
-                    ///>
-            //</div>
-        //</form>
-    //)
-   //ReactDOM.render(<Form />, document.querySelector("#form"))
-//};
 
 
