40fdd6692eaa6c9dfb1a97858eb67d31452f066b
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-09-16 20:25:22 +0000

    Sat, Sep 16, 2023, 4:25 PM -04:00

--- capstone/views.py
+++ capstone/views.py
@@ -156,14 +156,14 @@ def updateRecord(request, site_id):
 
     # find out if favorites record already exists. If not, create it
 
-        try:
-            favorite = Favorites.objects.get(
-                watcher__exact=folName, item__exact=favsite)
-        except Favorites.DoesNotExist:
-        # record not found so create record
+    try:
+        favorite = Favorites.objects.get(
+            watcher__exact=folName, item__exact=favsite)
+    except Favorites.DoesNotExist:
+    # record not found so create record
         #newRecord = True
         #activity = True
-            if activity == "true":
+        if activity == "true":
             print("got to look for favorites record")
             favorites = Favorites(watcher=folName,
                         item=favsite, is_active=True)
