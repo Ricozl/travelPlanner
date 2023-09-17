0826de6d773e83714d4a30c20ede5ea28396523a
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-09-17 19:02:57 +0000

    Sun, Sep 17, 2023, 3:02 PM -04:00

--- capstone/static/capstone/index.js
+++ capstone/static/capstone/index.js
@@ -88,7 +88,7 @@ function load_site(site) {
                         })
                     }
                     document.querySelector('#sites').append(element);
-                    })
+                    //})
                 }
         };
             console.log("got to focus")
