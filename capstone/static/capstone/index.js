5cc6601bbb024ffad3f12c5115ae1315eb93f7f8
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-09-17 18:56:44 +0000

    Sun, Sep 17, 2023, 2:56 PM -04:00

--- capstone/static/capstone/index.js
+++ capstone/static/capstone/index.js
@@ -63,8 +63,8 @@ function load_site(site) {
                         //let html = `<button class="follow" id="follow" style="margin:10px;">Follow</button><button class="unfollow" id="unfollow">Unfollow</button>`;
                             //const btn = document.getElementById('lastp')
                         element.appendChild(jump);
-                        // add event listener for "follow" and "unfollow" buttons
-                        element.addEventListener('click', event => {
+                        // add event listener for addFav or remFav
+                        jump.addEventListener('click', event => {
                             checkRecord(event)
                         });
                             //btn.insertAdjacentHTML("afterend", html)jump.innerHTML = `<p id="addfav" style="display:inline-block; padding:10px;">Click to add to Favorites  </p><img src="static/capstone/red-heart.png" style="width:20px; height:20px;">`;
