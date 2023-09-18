bc3be53dadbc0f81f8b3d1c8136748cdd0a88ab5
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-09-18 19:21:22 +0000

    Mon, Sep 18, 2023, 3:21 PM -04:00

--- capstone/views.py
+++ capstone/views.py
@@ -6,7 +6,6 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.core import serializers
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import requires_csrf_token
 
 
