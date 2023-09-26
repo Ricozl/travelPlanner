a3797b5601df63e6e8c920f98a2fab9a636014a2
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-09-26 18:59:04 +0000

    Tue, Sep 26, 2023, 2:59 PM -04:00

--- capstone/static/capstone/index.js
+++ capstone/static/capstone/index.js
@@ -90,7 +90,7 @@ function load_site(site) {
 };
 
 function updateRecord(site_id, activity) {
-    // update 'content' in database
+    // update 'is_active' in database
     const csrftoken = getCookie('csrftoken');
     fetch(`/updateRecord/${site_id}`, {
         method: 'PUT',
