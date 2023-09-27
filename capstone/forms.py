e35a90cb228abb2aa25761ecbd2c24be52df73d0
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-09-27 17:27:54 +0000

    Wed, Sep 27, 2023, 1:27 PM -04:00

--- capstone/forms.py
+++ capstone/forms.py
@@ -2,5 +2,5 @@ form django import forms
 
 class UserForm(forms.Form):
     username = forms.CharField(min_length=5, max_length=20)
-    email = forms.Email
-    password = forms.Password
+    email = forms.EmailField()
+    password = forms.PasswordField()
