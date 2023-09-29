26b0d14c6fb7aaef387f50c37815ef0c763df3e7
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-09-29 16:38:49 +0000

    Fri, Sep 29, 2023, 12:38 PM -04:00

--- capstone/views.py
+++ capstone/views.py
@@ -23,7 +23,7 @@ def sites(request, site):
     if site == "favorites":
         #items = Favorites.objects.filter(watcher__exact=request.user.id).filter(is_active__exact=True)
         #print(items)
-        values = Favorites.objects.filter(watcher__exact=request.user.id).values_list(is_active=True)
+        values = Favorites.objects.filter(watcher__exact=request.user.id).filter(is_active__exact=True).values_list()
         print(values)
         sites = Sites.objects.filter(id__in=list(values))
         #sites = Sites.objects.filter(id__in=Favorites.objects.filter(watcher__exact=request.user.id).filter(is_active__exact=True))
