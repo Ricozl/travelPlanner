5c35d00d6ffca83188493eef16f70109b01ed6e2
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-09-29 13:22:45 +0000

    Fri, Sep 29, 2023, 9:22 AM -04:00

--- capstone/static/capstone/index.js
+++ capstone/static/capstone/index.js
@@ -52,38 +52,41 @@ function load_site(site) {
                     // for signed-in Users create Add and Remove Favorites
                     userName = document.getElementById('user_name')
                     if (userName) {
-                        const jump = document.createElement('p');
-                        jump.innerHTML = `<p id="addfav" style="display:inline-block; padding:10px;">Click to add to Favorites  </p><img src="static/capstone/red-heart.png" style="width:20px; height:20px;">`;
-                        element.appendChild(jump);
+                        if (favFlag == False) {
+                            const jump = document.createElement('p');
+                            jump.innerHTML = `<p id="addfav" style="display:inline-block; padding:10px;">Click to add to Favorites  </p><img src="static/capstone/red-heart.png" style="width:20px; height:20px;">`;
+                            element.appendChild(jump);
 
-                        // add event listener for clicking on a site
-                        jump.addEventListener('click', function(e) {
-                            const element = e.target;
-                            element.previousSibling.innerText = "Site is in your Favorites List!"
+                            // add event listener for clicking on a site
+                            jump.addEventListener('click', function(e) {
+                                const element = e.target;
+                                element.previousSibling.innerText = "Site is in your Favorites List!"
 
-                            // update 'favorites' in database
-                            var site_id = parseInt(data[i].id)
-                            title = data[i].title
-                            // update favorites table in database, set to active
-                            activity = true
-                            updateRecord(site_id, activity)
-                        })
-                        const jumptwo = document.createElement('p');
-                        jumptwo.innerHTML = `<p id="remfav" style="display:inline-block; padding:10px;">Click to remove from Favorites  </p><img src="static/capstone/open-heart.png" style="width:20px; height:20px;">`;
-                        element.appendChild(jumptwo);
+                                // update 'favorites' in database
+                                var site_id = parseInt(data[i].id)
+                                title = data[i].title
+                                // update favorites table in database, set to active
+                                activity = true
+                                updateRecord(site_id, activity)
+                            })
+                        } else {
+                            const jumptwo = document.createElement('p');
+                            jumptwo.innerHTML = `<p id="remfav" style="display:inline-block; padding:10px;">Click to remove from Favorites  </p><img src="static/capstone/open-heart.png" style="width:20px; height:20px;">`;
+                            element.appendChild(jumptwo);
 
-                        // add event listener for clicking on a site
-                        jumptwo.addEventListener('click', function(e) {
-                            const element = e.target;
-                            element.previousSibling.innerText = "Site is not in your Favorites List!"
-                            // update 'favorites' in database
+                            // add event listener for clicking on a site
+                            jumptwo.addEventListener('click', function(e) {
+                                const element = e.target;
+                                element.previousSibling.innerText = "Site is not in your Favorites List!"
+                                // update 'favorites' in database
 
-                            var site_id = parseInt(data[i].id)
-                            title = data[i].title
-                            // update favorites table in database, set to inactive
-                            activity = false
-                            updateRecord(site_id, activity)
-                        })
+                                var site_id = parseInt(data[i].id)
+                                title = data[i].title
+                                // update favorites table in database, set to inactive
+                                activity = false
+                                updateRecord(site_id, activity)
+                            })
+                        }
                     }
                     document.querySelector('#sites').append(element);
                 }
