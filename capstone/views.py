1af7c46e83346824d46ac3ec5966dc691f0e815f
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-09-29 17:02:03 +0000

    Fri, Sep 29, 2023, 1:02 PM -04:00

--- capstone/views.py
+++ capstone/views.py
@@ -21,7 +21,8 @@ def quiz(request):
 
 def sites(request, site):
     if site == "favorites":
-        items = Favorites.objects.filter(watcher__exact=request.user.id) & (is_active == True)
+       
+        items = Favorites.objects.filter(watcher=request.user.id, is_active=True).values_list('watched')
         print(items)
         #qs1 = Sites.objects.values_list("id")
         #qs2 = Favorites.objects.values_list("watcher" & "is_)
