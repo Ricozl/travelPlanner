f76b0c33d0abf1439699195007b0cc950a439404
Ricozl <63819642+Ricozl@users.noreply.github.com>
2023-10-04 21:09:41 +0000

    Wed, Oct 4, 2023, 5:09 PM -04:00

--- capstone/static/capstone/react.js
+++ capstone/static/capstone/react.js
@@ -9,8 +9,7 @@ function App() {
         'The Colosseum is called that because?',
         'In classical architecture, Romans invented?',
         'Traditional Roman food is largely composed of?',
-        'One hundred years ago, Romans avoided artichokes
-         and eggplants because?'
+        '100 years ago, Romans avoided artichokes & eggplants because?'
     ]
 
     const answerList = [
