e6fe1024814e612cfb80cdbcc94431e440372583
Ricozl <suzeelan@comcast.net>
2024-06-17 22:16:16 +0000

    Jun 17, 2024, 6:16 PM

--- project5/settings.py
+++ project5/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '13kl@xtukpwe&xj2xoysxe9_6=tf@f8ewxer5n&ifnd46+6$%8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost',]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS = ['https://ricozl-fantastic-zebra-7x6j7wp9p9fxg94-8000.preview.app.github.dev',
                         'https://localhost:8000']
